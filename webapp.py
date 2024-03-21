094d9b5da693991165f0f441c97750ffe42ba109
Kinster <fantomchannel68@gmail.com>
2024-03-21 17:56:07 +0200

    changed webapp

--- webapp.py
+++ webapp.py
@@ -1,26 +1,10 @@
-# webapp.py
-from flask import Flask, render_template, jsonify, request
-import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def webapp_index():
-    user_data = request.args.get('user_data', default=None, type=str)
-    return render_template('index.html', user_data=user_data)
-
-@app.route('/get_level')
-def get_level():
-    user_data = request.args.get('user_data', default=None, type=str)
-    if user_data:
-        user_data = user_data.replace('\'', '\"')
-        user_data = json.loads(user_data)
-
-        user_level = user_data.get('user-lvl', None)
-        if user_level:
-            return jsonify(level=user_level)
-
-    return jsonify(error=True)
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
